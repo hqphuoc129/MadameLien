8b2b1ee8b425c739079d06a78e5586a3c04d5be5
hqphuoc129 <hqphuoc129@gmail.com>
2022-02-15 16:51:27 +0700

    add screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,18 +13,10 @@ window.addEventListener("click", function(e) {
 
 // Slick slider
 // $(document).ready(function () {
-//   $(".quote-list").slick({
+//   $(".modallist").slick({
 //     prevArrow:
 //       "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
 //     nextArrow:
 //       "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
-//     responsive: [
-//       {
-//         breakpoint: 767,
-//         settings: {
-//           arrows: false,
-//         },
-//       },
-//     ],
 //   });
 // });
