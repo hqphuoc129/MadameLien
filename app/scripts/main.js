bb64a15c81699c154f0a9696e9df9c36454235d5
hqphuoc129 <hqphuoc129@gmail.com>
2022-02-16 12:27:52 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,11 +12,11 @@ window.addEventListener("click", function(e) {
 
 
 // Slick slider
-// $(document).ready(function () {
-//   $(".modallist").slick({
-//     prevArrow:
-//       "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
-//     nextArrow:
-//       "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
-//   });
-// });
+$(document).ready(function () {
+  $(".quote-container").slick({
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
