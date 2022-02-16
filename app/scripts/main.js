f44920857139ed1c88738feb14f69e21c98f48da
hqphuoc129 <hqphuoc129@gmail.com>
2022-02-16 16:15:17 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,25 @@
 const menuToggle = document.querySelector(".header-toggle");
 const menuHeader = document.querySelector(".header-menu");
 const expandClass = "is-expand";
-menuToggle.addEventListener("click", function() {
+menuToggle.addEventListener("click", function () {
   menuHeader.classList.add(expandClass);
 });
-window.addEventListener("click", function(e) {
-  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")){
+window.addEventListener("click", function (e) {
+  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
     menuHeader.classList.remove(expandClass);
   }
 });
-
-
 // Slick slider
 $(document).ready(function () {
   $(".quote-container").slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
     prevArrow:
       "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:
---
+++ app/scripts/scriptquote.js
@@ -0,0 +1,43 @@
+const zoomBtn = document.querySelectorAll('.zoom-text');
+const allImages = document.querySelectorAll('.mediaquote-item');
+const imageView = document.querySelector('.image-view');
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+const imageBox = document.querySelector('.image-box');
+
+let currentImageIdx = 0;
+
+imageView.addEventListener('click', function(){
+    this.style.display = "none";
+    imageBox.style.display = "none";
+})
+
+
+zoomBtn.forEach(function(btn, index){
+    btn.addEventListener('click', function(){
+        imageView.style.display = "block";
+        imageBox.style.display = "block";
+        currentImageIdx = index + 1;
+        currentImageDisplay(currentImageIdx);
+    })
+})
+
+function currentImageDisplay(position){
+    imageBox.style.background = `url(../images/img-mediaquote${currentImageIdx}.png) center/cover no-repeat`;
+}
+
+prevBtn.addEventListener('click', function(){
+    currentImageIdx--;
+    if(currentImageIdx === 0){
+        currentImageIdx = allImages.length;
+    }
+    currentImageDisplay(currentImageIdx);
+})
+
+nextBtn.addEventListener('click', function(){
+    currentImageIdx++;
+    if(currentImageIdx === 10){
+        currentImageIdx = 1;
+    }
+    currentImageDisplay(currentImageIdx);
+})
